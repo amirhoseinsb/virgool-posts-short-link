446a121a7a4e562d37a00ce472b948eeafcc9c72
amirhosein sohrabi <amirhoseinsohrabi.official@gmail.com>
2022-08-18 20:32:24 +0430

    add run script file

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 14 19:30:54 2022
+
+@author: amir
+"""
+from vpl import VirgoolPostsLink
+from vpsl import VirgoolPostsShortLink
+from banner import HELP,WELCOME
+from sys import exit 
+
+def run_vpl():
+    post = VirgoolPostsLink()    
+    post.request_to_site()    
+    post.scroll_down()    
+    post.find_links()    
+    post.extract_link()    
+    post.write_in_disk()    
+    post.close_driver()
+
+def run_vpsl():
+    post = VirgoolPostsShortLink()
+    post.file_input()
+    post.request_to_site()
+    post.extract_link()
+    post.write_in_disk()
+    
+if __name__ == '__main__':
+    _ = input(WELCOME)
+    if _.lower() == 'x':
+        exit()
+    elif int(_) == 1:
+        run_vpl()
+    elif int(_) == 2:
+        run_vpsl()
+
