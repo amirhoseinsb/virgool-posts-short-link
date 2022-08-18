fbda8bdac40f62b27aaf3b5aefcd3a0c60b5464a
amirhosein sohrabi <amirhoseinsohrabi.official@gmail.com>
2022-08-18 20:31:02 +0430

    add banner

---
+++ banner.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 22:18:32 2022
+
+@author: amir
+"""
+BANNER_VPL = """
+__     ______  _     
+\ \   / /  _ \| |    
+ \ \ / /| |_) | |    
+  \ V / |  __/| |___ 
+   \_/  |_|   |_____|  Virgool Posts Link 
+
+-------------------------------------------
+Created By : AmirhoseinSohrabi
+Gmail : amirhoseinsohrabi.official@gmail.com
+-------------------------------------------
+
+X = EXIT
+
+Please Enter The UserName : """
+
+BANNER_VPSL = """
+
+__     ______  ____  _     
+\ \   / /  _ \/ ___|| |    
+ \ \ / /| |_) \___ \| |    
+  \ V / |  __/ ___) | |___ 
+   \_/  |_|   |____/|_____| Virgool Posts Short Link
+
+-------------------------------------------
+Created By : AmirhoseinSohrabi
+Gmail : amirhoseinsohrabi.official@gmail.com
+-------------------------------------------
+
+X = EXIT
+
+Please Enter The Directory Name : """
+
+FILESAVE = "Please Enter The Name For Save the Result :"
+
+
+
+WELCOME = """
+
+__        _______ _     ____ ___  __  __ _____ 
+\ \      / / ____| |   / ___/ _ \|  \/  | ____|
+ \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|  
+  \ V  V / | |___| |__| |__| |_| | |  | | |___ 
+   \_/\_/  |_____|_____\____\___/|_|  |_|_____|
+                                               
+Welcome To the Script 
+
+1 = Virgool posts Link
+2 = Virgool Posts Short Link
+X = Exit
+
+-----------------------------------------------
+Enter The Number :"""
